bebc3954c989b3b13cbfe700c17f0029cc78e772
AndrewG <andrey_g.2019@mail.ru>
2025-12-24 20:56:48 +0700

    Add Contractor service interface and dto's for contractors and agreement.
    TODO:
    AgreementDto
    ContractorDto
    IndividualContractorDto
    LegalEntityContractorDto

---
+++ PropertyManagmentSystem/Application/Interfaces/IContractorService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyManagmentSystem.Application.DTOs;
+
+namespace PropertyManagmentSystem.Application.Interfaces
+{
+    public interface IContractorService
+    {
+        // Общие методы для всех арендаторов
+        ContractorDto GetContractorById(int id);
+        IEnumerable<ContractorDto> GetAllContractors();
+        void AddContractor(ContractorDto contractorDto);
+        void UpdateContractor(ContractorDto contractorDto);
+        void DeactivateContractor(int id, string reason);
+        void ActivateContractor(int id);
+
+        // Физические лица
+        void AddIndividualContractor(IndividualContractorDto dto);
+        void UpdateIndividualContractor(IndividualContractorDto dto);
+
+        // Юридические лица
+        void AddLegalEntityContractor(LegalEntityContractorDto dto);
+        void UpdateLegalEntityContractor(LegalEntityContractorDto dto);
+
+        // Договоры арендатора
+        IEnumerable<AgreementDto> GetContractorAgreements(int contractorId);
+        bool CanContractorCreateNewAgreement(int contractorId);
+    }
+}
